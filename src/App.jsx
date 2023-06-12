5a160e6676523afa920ddb39df7292fe906553b8
Eddie Kao <eddie@5xcampus.com>
2023-06-12 19:04:48 +0800

    範例下載

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1 className="heading">五倍紙牌遊戲</h1>
+      <div className="flex">
+        <button className="restart">New Game</button>
+      </div>
+      <div className="container"></div>
+    </>
+  )
+}
+
+export default App
